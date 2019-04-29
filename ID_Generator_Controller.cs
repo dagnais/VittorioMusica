59ae8be75674424a3f527439dc25621accb1db05
dagnais <40569950+dagnais@users.noreply.github.com>
2019-04-29 20:08:00 -0300

    VittorioMusica
    Aplicación con conexión mySql

---
+++ ID_Generator_Controller.cs
@@ -0,0 +1,67 @@
+﻿/*
+Titulo: "VittorioMusica"
+Hecho en el año:2018 
+-----
+Title: "VittorioMusica"
+Made in the year: 2018
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ID_Generator_Controller : MonoBehaviour
+{
+    public bool isSaveIndexs;
+    public Dictionary<string, int> myIDs= new Dictionary<string, int>();
+    public MySQL_Script mySqlDB;
+
+    void Start()
+    {
+        IndexsInicialization();
+    }
+
+    void IndexsInicialization()
+    {
+        if (!mySqlDB.isConnInternet)
+        {
+            myIDs.Add(DataBaseEstructure.tableStock, PlayerPrefs.GetInt(DataBaseEstructure.tableStock, mySqlDB.GetIndex(DataBaseEstructure.tableStock,
+                DataBaseEstructure.idStock)));
+            myIDs.Add(DataBaseEstructure.tableCodigo, PlayerPrefs.GetInt(DataBaseEstructure.tableCodigo, mySqlDB.GetIndex(DataBaseEstructure.tableCodigo,
+                DataBaseEstructure.idCodigo)));
+        }
+        else
+        {
+            myIDs.Add(DataBaseEstructure.tableStock, mySqlDB.GetIndex(DataBaseEstructure.tableStock,
+                DataBaseEstructure.idStock));
+            myIDs.Add(DataBaseEstructure.tableCodigo, mySqlDB.GetIndex(DataBaseEstructure.tableCodigo,
+                DataBaseEstructure.idCodigo));
+        }
+        SaveIndexs(DataBaseEstructure.tableStock, myIDs[DataBaseEstructure.tableStock]);
+        SaveIndexs(DataBaseEstructure.tableCodigo, myIDs[DataBaseEstructure.tableCodigo]);
+    }
+
+    public int GenerateID(string table)
+    {
+        int value= PlayerPrefs.GetInt(table,0);
+        Debug.Log("value: " + value);
+
+        if (myIDs.TryGetValue(table, out value))
+        {
+            value++;
+            myIDs[table]= value;
+            Debug.Log("Index añadido con éxito: "+ value);
+            return value;
+        }
+        else
+        {
+            Debug.LogError("tabla no encontrada <diccionario>");
+            return 0;
+        }
+    }
+
+    public void SaveIndexs(string table,int lastIndex)
+    {
+
+        PlayerPrefs.SetInt(table, lastIndex);
+    }
+}
---
+++ SetDataBase_Controller.cs
@@ -0,0 +1,84 @@
+﻿/*
+Titulo: "VittorioMusica"
+Hecho en el año:2018 
+-----
+Title: "VittorioMusica"
+Made in the year: 2018
+*/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDataBase_Controller : MonoBehaviour
+{
+    public GameObject[] sceneObjs;
+    public InputField[] dataInputs;
+
+    public MySQL_Script mysql;
+    public Text consoleText;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Habilitado", 0) == 1)
+        {
+            mysql.LoadConnection();
+            DoneValidation();
+        }
+    }
+
+    public void DataValidate()
+    {
+        mysql.server= dataInputs[0].text;
+        mysql.dataBase = dataInputs[1].text;
+        mysql.user= dataInputs[2].text;
+        mysql.password= dataInputs[3].text;
+        mysql.SaveConnection();
+        consoleText.text = "Conectando con el servidor...";
+        Invoke("Validating", 1);
+    }
+    void Validating()
+    {
+        bool value = mysql.CheckConnection();
+        if (value)
+        {
+            DoneValidation();
+        }
+        else
+        {
+            Reset();
+            consoleText.text = "Fallo de conexion, revise los datos ingresados.";
+            Debug.LogError("Fallo de conexion");
+        }
+    }
+
+    void DoneValidation()
+    {
+        for (int i = 0; i < sceneObjs.Length; i++)
+        {
+            sceneObjs[i].SetActive(true);
+        }
+        gameObject.SetActive(false);
+        PlayerPrefs.SetInt("Habilitado", 1);
+
+        mysql.DBConnect();
+        mysql.CloseConection();
+        mysql.DB_SelectStock(true, null);
+    }
+
+    //recibe los datos del select
+    public void ReceiveResponse(string[] result)
+    {
+        mysql.StockSelectTable(result);
+    }
+
+    public void Reset()
+    {
+        for (int i = 0; i < sceneObjs.Length; i++)
+        {
+            sceneObjs[i].SetActive(false);
+        }
+        for (int i = 0; i < dataInputs.Length; i++)
+        {
+            dataInputs[i].text = string.Empty;
+        }
+    }
+}
